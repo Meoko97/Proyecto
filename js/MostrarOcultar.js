b396ccaa08d7e7eb47924adbe0e6f4d7b2482a44
Meoko_97 <dgarmar97@gmail.com>
2017-12-06 19:50:59 +0100

    initial comit

---
+++ js/MostrarOcultar.js
@@ -0,0 +1,8 @@
+var element = document.getElementById("contenido");
+var check = document.getElementById("check");
+if (check.checked) {
+    element.style.display='block';
+}
+else {
+    element.style.display='none';
+}
